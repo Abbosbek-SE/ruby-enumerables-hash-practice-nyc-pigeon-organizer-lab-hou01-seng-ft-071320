b1bca524594cac0541876bddf2ea2476155a4214
Abbosbek Muydinboev <muydinboevabbosbek@gmail.com>
2020-07-05 06:47:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,10 +3,15 @@ def nyc_pigeon_organizer(data)
   for key, value in data do 
     for inner_key, inner_value in value do
       inner_value.collect do |i|
-        hash[i] = {key => inner_key}
+        if !hash[i]
+          hash[i] = {}
+        end
+        if !hash[i]
+          hash[i] = key
+        end
       end
     end
   end
-  return hash
+  hash
 end
 
