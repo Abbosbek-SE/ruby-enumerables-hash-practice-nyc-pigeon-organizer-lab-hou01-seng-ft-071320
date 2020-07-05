442ccfd519e1e2591fa0873adaf5516ea97fa2c1
Abbosbek Muydinboev <muydinboevabbosbek@gmail.com>
2020-07-05 06:50:26 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = Hash.new 
+  for key, value in data do 
+    for inner_key, inner_value in value do
+      inner_value.collect do |i|
+        if !hash[i]
+          hash[i] = {}
+        end
+        if !hash[i][key]
+          hash[i][key] = []
+        end
+        hash[i][key].push(inner_key.to_s)
+      end
+    end
+  end
+  hash
 end
+
